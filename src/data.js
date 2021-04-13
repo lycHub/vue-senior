63225b0cc46467c0f69d1b694dd62bbcc240bf74
liyichao <1120805364@qq.com>
2021-04-13 10:19:42 +0800

    picker demo

--- src/data.js
+++ src/data.js
@@ -105,3 +105,77 @@ export const treeData = [
     ]
   }
 ];
+export const singlePicker = [
+  {
+    value: 'a0',
+    label: '东城区'
+  },
+  {
+    value: 'a1',
+    label: '西城区'
+  },
+  {
+    value: 'a2',
+    label: '崇文区'
+  },
+  {
+    value: 'a3',
+    label: '宣武区'
+  },
+  {
+    value: 'a4',
+    label: '朝阳区'
+  },
+  {
+    value: 'a5',
+    label: '丰台区'
+  },
+  {
+    value: 'a6',
+    label: '石景山区'
+  },
+  {
+    value: 'a7',
+    label: '海淀区'
+  },
+  {
+    value: 'a8',
+    label: '门头沟区'
+  },
+  {
+    value: 'a9',
+    label: '房山区'
+  },
+  {
+    value: 'a10',
+    label: '通州区'
+  },
+  {
+    value: 'a11',
+    label: '顺义区'
+  },
+  {
+    value: 'a12',
+    label: '昌平区'
+  },
+  {
+    value: 'a13',
+    label: '大兴区'
+  },
+  {
+    value: 'a14',
+    label: '平谷区'
+  },
+  {
+    value: 'a15',
+    label: '怀柔区'
+  },
+  {
+    value: 'a16',
+    label:  '密云县'
+  },
+  {
+    value: 'a17',
+    label: '延庆县'
+  }
+];
